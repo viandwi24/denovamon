f451d2f535129f54af3e6ba4eba44f60f9b1e168
viandwi24 <fiandwi0424@gmail.com>
2020-06-03 04:53:21 +0700

    update

--- run.ts
+++ run.ts
@@ -5,17 +5,14 @@ const parsedArgs: StdFlags.Args = StdFlags.parse(Deno.args);
 const root = Deno.cwd();
 
 if (parsedArgs._[0] == "start") {
-    let configFromFile: boolean = (getValue("command", null) == null) ? true : false;
-    await start(configFromFile);
+    await start();
 } else if (parsedArgs._[0] == "version" || typeof parsedArgs.v != 'undefined') {
     console.log(Iro.default(`Denovamon V${version}`, Iro.bold, Iro.green));
 }
 Deno.exit();
 
-async function start(configFromFile: boolean) {
-    let options: IOptions = (configFromFile) ? {
-        configFile: getValue("config", "denovamon.json")
-    } : {
+async function start() {
+    let options: IOptions = {
         path: Deno.cwd(),
         command:  getValue("command", "deno run -A app.ts"),
         clearOnRestart: getValue("clear-on-restart", false),
