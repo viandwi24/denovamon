364ed419bfe2a1dbc37d063af5cee06a703c0c3b
viandwi24 <fiandwi0424@gmail.com>
2020-06-03 04:51:05 +0700

    added version info

--- mod.ts
+++ mod.ts
@@ -1,2 +1,3 @@
 // Copyright 2020 the Denovamon authors. All rights reserved. MIT license.
 export * from "./denovamon.ts";
+export const version = "0.1.1";
--- run.ts
+++ run.ts
@@ -1,15 +1,21 @@
-import { Denovamon, IOptions } from "./mod.ts";
-import { StdFlags } from "./deps.ts";
+import { Denovamon, IOptions, version } from "./mod.ts";
+import { StdFlags, Iro } from "./deps.ts";
 
-const parsedArgs = StdFlags.parse(Deno.args);
+const parsedArgs: StdFlags.Args = StdFlags.parse(Deno.args);
 const root = Deno.cwd();
 
 if (parsedArgs._[0] == "start") {
-    await start();
+    let configFromFile: boolean = (getValue("command", null) == null) ? true : false;
+    await start(configFromFile);
+} else if (parsedArgs._[0] == "version" || typeof parsedArgs.v != 'undefined') {
+    console.log(Iro.default(`Denovamon V${version}`, Iro.bold, Iro.green));
 }
+Deno.exit();
 
-async function start() {
-    let options: IOptions = {
+async function start(configFromFile: boolean) {
+    let options: IOptions = (configFromFile) ? {
+        configFile: getValue("config", "denovamon.json")
+    } : {
         path: Deno.cwd(),
         command:  getValue("command", "deno run -A app.ts"),
         clearOnRestart: getValue("clear-on-restart", false),
